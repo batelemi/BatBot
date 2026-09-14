7520c1a161ef3092861b5bc289272af577f89627
batelemi <doelugu11@gmail.com>
2026-09-14 23:00:52 +0000

    Correction du chemin du dossier frontend

--- server.js
+++ server.js
@@ -505,10 +505,10 @@ app.patch("/api/admin/settings", requireAdmin, (req, res) => {
   res.json({ message: "Configuration enregistrée.", settings: getSettings() });
 });
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "publique")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "public", "index.html"));
+  res.sendFile(path.join(__dirname, "publique", "index.html"));
 });
 
 app.listen(PORT, "0.0.0.0", () => {
