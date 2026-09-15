63d79c159ec7ec4815ad788c2bbbc46e2ae2494a
batelemi <doelugu11@gmail.com>
2026-09-15 05:35:51 +0000

    Fix static file directory from 'publique' to 'public'

--- server.js
+++ server.js
@@ -578,10 +578,10 @@ app.patch("/api/admin/settings", requireAdmin, (req, res) => {
   res.json({ message: "Configuration enregistrée.", settings: getSettings() });
 });
 
-app.use(express.static(path.join(__dirname, "publique")));
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "publique", "index.html"));
+  res.sendFile(path.join(__dirname, "public", "index.html"));
 });
 
 app.listen(PORT, () => {
