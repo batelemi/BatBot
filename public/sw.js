4323b824fa0d88074d19b964096c444976d1b618
batelemi <doelugu11@gmail.com>
2026-09-16 22:50:06 +0000

    Mise à jour interface S-Drive et ajout du logo

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE = "s-drive-v2";
+const CACHE = "s-drive-v1";
 const ASSETS = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
 
 self.addEventListener("install", event => {
